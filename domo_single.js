2edd3b4bcb5ae37fb1a2871a39c34be00e17844b
alexmenko <alexavramenko1@Gmail.com>
2017-01-30 11:00:39 -0500

    Update domo_single.js

--- domo_single.js
+++ domo_single.js
@@ -8,7 +8,7 @@ var REPORT_TYPE = 'KEYWORDS_PERFORMANCE_REPORT';
 
 var FILTER = 'Impressions > 0';
 
-var DATE_RANGE = 'LAST_7_DAYS';
+var DATE_RANGE = 'thismonth';
 	Logger.log("Report date set");
 
 var COLUMN_NAMES = [
