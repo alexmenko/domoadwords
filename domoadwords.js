c82f7e20db90cac529562404502ce06385ffd705
alexmenko <alexavramenko1@Gmail.com>
2017-01-31 12:05:38 -0500

    Update domoadwords.js

--- domoadwords.js
+++ domoadwords.js
@@ -3,7 +3,7 @@
 
 	Logger.log("Initiating remote script");
 
-var REPORT_TYPE = 'FINAL_URL_REPORT';
+var REPORT_TYPE = 'KEYWORDS_PERFORMANCE_REPORT';
 	Logger.log("Report type set");
 
 var FILTER = 'Impressions > 0';
@@ -12,24 +12,43 @@ var DATE_RANGE = 'THIS_MONTH';
 	Logger.log("Report date set");
 
 var COLUMN_NAMES = [
+	'Date',
+	'Criteria',
+	'Status',
 	'CampaignName',
-	'EffectiveFinalUrl',
 	'AdGroupName',
 	'Clicks',
 	'Impressions',
 	'Ctr',
 	'AverageCpc',
 	'AverageCpm',
+  'CpcBid',
 	'Cost',
 	'AveragePosition',
 	'CostPerConversion',
 	'ConversionRate',
 	'Conversions',
+	'ViewThroughConversions',
+	'QualityScore',
+	'BounceRate',
+	'CpcBid',
+	'AveragePageviews',
+	'AverageTimeOnSite'
 ];
 
 var COLUMNS = COLUMN_NAMES.join(',');
 	Logger.log("Columns set");
 
+var REPORT = AdWordsApp.report(
+	'SELECT ' +
+	COLUMNS +
+	' FROM ' +
+	REPORT_TYPE +
+	' WHERE Status IN [REMOVED, ENABLED, PAUSED]' +
+	' AND ' +
+	FILTER +
+	' DURING ' +
+	DATE_RANGE
 );
 	Logger.log("Report built");
 
