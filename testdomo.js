c3fc15f80c1d3c6680e574cdad9f7498facea664
alexmenko <alexavramenko1@Gmail.com>
2017-02-06 11:55:07 -0500

    Update testdomo.js

--- testdomo.js
+++ testdomo.js
@@ -3,24 +3,38 @@
 
 	Logger.log("Initiating remote script");
 
-var REPORT_TYPE = 'FINAL_URL_REPORT';
+var REPORT_TYPE = 'KEYWORDS_PERFORMANCE_REPORT';
 	Logger.log("Report type set");
 
 var FILTER = 'Impressions > 0';
 
-var DATE_RANGE = 'THIS_MONTH';
+var currentDate = new Date();
+var DATE_RANGE = "20170101, " + currentDate.getFullYear() + (((currentDate.getMonth() + 1).toString().length < 2) ? ("0" + (currentDate.getMonth() + 1)) : currentDate.getMonth() + 1) + (((currentDate.getDate()).toString().length < 2) ? ("0" + currentDate.getDate()) : currentDate.getDate());
 	Logger.log("Report date set");
 
 var COLUMN_NAMES = [
 	'Date',
+	'Criteria',
+	'Status',
 	'CampaignName',
-	'EffectiveFinalUrl',
+	'AdGroupName',
 	'Clicks',
 	'Impressions',
+	'Ctr',
+	'AverageCpc',
+	'AverageCpm',
+  'CpcBid',
 	'Cost',
 	'AveragePosition',
+	'CostPerConversion',
+	'ConversionRate',
 	'Conversions',
-
+	'ViewThroughConversions',
+	'QualityScore',
+	'BounceRate',
+	'CpcBid',
+	'AveragePageviews',
+	'AverageTimeOnSite'
 ];
 
 var COLUMNS = COLUMN_NAMES.join(',');
@@ -29,12 +43,14 @@ var COLUMNS = COLUMN_NAMES.join(',');
 var REPORT = AdWordsApp.report(
 	'SELECT ' +
 	COLUMNS +
+	' FROM ' +
+	REPORT_TYPE +
+	' WHERE Status IN [REMOVED, ENABLED, PAUSED]' +
 	' AND ' +
 	FILTER +
 	' DURING ' +
 	DATE_RANGE
 );
-
 	Logger.log("Report built");
 
 REPORT.exportToSheet(adwordsSheet);
